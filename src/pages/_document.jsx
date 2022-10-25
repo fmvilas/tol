f08d7a8f27977b03c3f42b8a707d34b4d74bcba7
Fran Mendez <fmvilas@gmail.com>
2022-10-25 16:59:22 +0200

    add google analytics

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -13,18 +13,13 @@ export default function Document() {
           rel="stylesheet"
           href="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,400&display=swap"
         />
-        <script dangerouslySetInnerHTML={{__html: `
-          var _paq = window._paq = window._paq || [];
-          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-          _paq.push(['trackPageView']);
-          _paq.push(['enableLinkTracking']);
-          (function() {
-            var u="https://tolshow.matomo.cloud/";
-            _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '1']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.async=true; g.src='//cdn.matomo.cloud/tolshow.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-          })();
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R688LSMZXH" />
+        <script dangerouslySetInnerHTML={{__html: `    
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-R688LSMZXH');
         `}}/>
       </Head>
       <body>
